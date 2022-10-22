f1eacd237cc760abc5a3ebe858af3153da434041
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-10-23 01:33:40 +0300

    Add tests.

---
+++ test/boot.php
@@ -0,0 +1,14 @@
+<?php
+// Run:
+// phpunit --bootstrap=./boot.php ./unit
+// phpunit --bootstrap=./boot.php ./unit --colors --testdox
+namespace KGun\Cryptee;
+
+// Register autoload.
+spl_autoload_register(function ($name) {
+    if (strpos($name, __NAMESPACE__) === 0) {
+        $name = strtr(substr($name, strlen(__NAMESPACE__)), '\\', '/');
+        $file = realpath(__DIR__ . '/../src/' . $name . '.php');
+        $file && require $file;
+    }
+});
---
+++ test/unit/CrypteeTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+use KGun\Cryptee\{Cryptee, CrypteeException};
+
+class CrypteeTest extends PHPUnit\Framework\TestCase
+{
+    const KEY = '4]%gmHo"e:]*hR(NQ?B...';
+
+    function test_constructor() {
+        new Cryptee(self::KEY); // OK.
+
+        $this->expectException(CrypteeException::class);
+        $this->expectExceptionMessageMatches('~Key length must be at least 6 chars and contain alp-num & printable chars~');
+        new Cryptee('abc');
+    }
+
+    function test_getters() {
+        $cryptee = new Cryptee(self::KEY);
+
+        $this->assertSame(self::KEY, $cryptee->key());
+        $this->assertSame(Cryptee::B64, $cryptee->type());
+    }
+
+    function test_encode() {
+        $cryptee = new Cryptee(self::KEY);
+
+        $this->assertSame('WJAv2/6x5Du/5IXjLTakW+jr', $cryptee->encode('Lorem ipsum dolor.'));
+    }
+
+    function test_decode() {
+        $cryptee = new Cryptee(self::KEY);
+        $content = 'Lorem ipsum dolor.';
+
+        $this->assertSame('Lorem ipsum dolor.', $cryptee->decode('WJAv2/6x5Du/5IXjLTakW+jr'));
+    }
+
+    function test_generateKey() {
+        $this->assertSame(Cryptee::KEY_LENGTH, strlen(Cryptee::generateKey()));
+        $this->assertSame(64, strlen(Cryptee::generateKey(64)));
+    }
+}
